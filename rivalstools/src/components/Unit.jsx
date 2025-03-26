a2cd4ccbe0751fa5338cb374d432d08013321143
simaaksidd <simaaksidd1@gmail.com>
2025-03-25 19:14:31 -0500

    add Unit component probably should change the name

--- rivalstools/src/components/Unit.jsx
+++ rivalstools/src/components/Unit.jsx
@@ -1,7 +1,19 @@
-function Unit() {
+import './styles/Unit.css'
+
+function Unit({ title = "", boxCount = 0 }) {
+    const boxes = Array.from({ length: boxCount });
     return (
-        <div></div>
-    )
-}
+        <div className="hero-wrapper">
+            <h3 className="hero-title">{title}</h3>
+            <div className="hero-container">
+                <div className="hero-grid">
+                    {boxes.map((_, index) => (
+                        <img src={'./public/hero_icons/adam_warlock.png'} key={index} className="hero-box"></img>
+                    ))}
+                </div>
+          </div> 
+        </div>
+    );
+  }
 
 export default Unit

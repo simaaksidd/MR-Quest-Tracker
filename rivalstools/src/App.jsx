984c610a1558f4a3fb819e22e058675217550c35
simaaksidd <simaaksidd1@gmail.com>
2025-03-25 19:15:03 -0500

    add Unit component probably should change the name

--- rivalstools/src/App.jsx
+++ rivalstools/src/App.jsx
@@ -1,13 +1,22 @@
 //import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import Board from './components/board'
+import Unit from './components/Unit'
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 class="Title">Rivals Tools</h1>
-    <div></div>
+    <Navbar></Navbar>
+    <h1>Rivals Tools</h1>
+    <Board></Board>
+    <div className="hero-section">
+      <Unit title="Vanguard" boxCount={9}></Unit>
+      <Unit title="Duelist" boxCount={20}></Unit>
+      <Unit title="Strategist" boxCount={8}></Unit>
+    </div>
     </>
   )
 }

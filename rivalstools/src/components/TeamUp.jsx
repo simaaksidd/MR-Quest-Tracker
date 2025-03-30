8607a515ad1e8b5a20221857c66f1a5a043301f6
simaaksidd <simaaksidd1@gmail.com>
2025-03-30 17:37:40 -0500

    add teamup component

---
+++ rivalstools/src/components/TeamUp.jsx
@@ -0,0 +1,11 @@
+import './styles/TeamUp.css';
+
+function TeamUp() {
+  return (
+    <div className='team-up-section'>
+        <h2 className='team-up-heading'>Team Up Abilities</h2>
+    </div>
+  )
+}
+
+export default TeamUp;
